aa59871d83f384c74abb83af429f20923c73dbc2
mbed92 <michalbednarek.e@gmail.com>
2019-10-01 14:03:51 +0200

    final version of 2-finger gripper

--- create_dataset.py
+++ create_dataset.py
@@ -71,7 +71,7 @@ def log_into_file(args):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--sim-step', type=int, default=10)
-    parser.add_argument('--vis', type=bool, default=False)
+    parser.add_argument('--vis', type=bool, default=True)
     parser.add_argument('--mask-contact', type=bool, default=False)
     parser.add_argument('--sim-start', type=int, default=1)
     parser.add_argument('--data-folder', type=str, default="./data/dataset/ds_IMU_with_contact_sense_full_two_fingers")
