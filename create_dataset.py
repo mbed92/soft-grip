f98a08bff8d87d34e05c04a1a51744da793a5b0a
mbed92 <michalbednarek.e@gmail.com>
2019-10-01 14:20:52 +0200

    minor fixes

--- create_dataset.py
+++ create_dataset.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     parser.add_argument('--vis', type=bool, default=False)
     parser.add_argument('--mask-contact', type=bool, default=False)
     parser.add_argument('--sim-start', type=int, default=1)
-    parser.add_argument('--data-folder', type=str, default="./data/dataset/ds_IMU_with_contact_sense_full_two_fingers")
+    parser.add_argument('--data-folder', type=str, default="./data/dataset/ds_IMU_with_contact_sense_full_two_fingers_v1")
     parser.add_argument('--data-name', type=str, default="train_dataset")
     parser.add_argument('--mujoco-model-paths', nargs="+", required=True)
     args, _ = parser.parse_known_args()
--- environment/manenv.py
+++ environment/manenv.py
@@ -98,7 +98,7 @@ class ManEnv(Env):
         for i in range(2):
             self.env.data.ctrl[i] = 0.0
 
-    def set_new_stiffness(self, range_min=1e-1, range_max=10.0):
+    def set_new_stiffness(self, range_min=1e-2, range_max=2.0):
         new_value = np.random.uniform(range_min, range_max)
         for i in self.joint_ids:
             self.env.model.jnt_stiffness[i] = new_value
